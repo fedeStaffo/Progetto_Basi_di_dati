4b891478e1ad25bde13dc31c4b28d846ed7a8dae
Federico Staffolani <94174873+fedeStaffo@users.noreply.github.com>
2022-12-05 17:55:59 +0100

    aggiunto database e codice tabelle

---
+++ progetto_ticket.sql
@@ -0,0 +1,356 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3308
+-- Creato il: Dic 05, 2022 alle 17:54
+-- Versione del server: 10.4.27-MariaDB
+-- Versione PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `progetto_ticket`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `Partita_IVA` int(11) NOT NULL,
+  `Nome` varchar(20) NOT NULL,
+  `Stato` varchar(20) NOT NULL,
+  `Città` varchar(20) DEFAULT NULL,
+  `Saldo` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `consumabile`
+--
+
+CREATE TABLE `consumabile` (
+  `IDConsumabile` int(12) NOT NULL,
+  `Categoria` varchar(15) NOT NULL,
+  `Prezzo` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `contratto`
+--
+
+CREATE TABLE `contratto` (
+  `IDContratto` int(5) NOT NULL,
+  `DataInizio` date NOT NULL,
+  `DataFine` date NOT NULL,
+  `Canone` int(4) NOT NULL,
+  `Banca` varchar(20) NOT NULL,
+  `Cliente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `datianagrafici`
+--
+
+CREATE TABLE `datianagrafici` (
+  `CodiceFiscale` char(16) NOT NULL,
+  `Nome` varchar(20) NOT NULL,
+  `Cognome` varchar(20) NOT NULL,
+  `Sesso` char(1) NOT NULL,
+  `DataNascita` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `datilavorativi`
+--
+
+CREATE TABLE `datilavorativi` (
+  `IDTecnico` int(4) NOT NULL,
+  `Resident` varchar(2) NOT NULL,
+  `CF` char(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `emailcliente`
+--
+
+CREATE TABLE `emailcliente` (
+  `NumCliente` int(11) NOT NULL,
+  `Email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `emailtecnico`
+--
+
+CREATE TABLE `emailtecnico` (
+  `NumTecnico` int(4) NOT NULL,
+  `Email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `garanzia`
+--
+
+CREATE TABLE `garanzia` (
+  `IDGaranzia` int(7) NOT NULL,
+  `DataInstallazione` date NOT NULL DEFAULT current_timestamp(),
+  `LuogoInstallazione` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `macchinario`
+--
+
+CREATE TABLE `macchinario` (
+  `IDMacchinario` int(12) NOT NULL,
+  `Categoria` varchar(15) NOT NULL,
+  `Gar` int(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `telcliente`
+--
+
+CREATE TABLE `telcliente` (
+  `NumCliente` int(11) NOT NULL,
+  `Telefono` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `teltecnico`
+--
+
+CREATE TABLE `teltecnico` (
+  `NumTecnico` int(4) NOT NULL,
+  `Telefono` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `IDTicket` int(10) NOT NULL,
+  `Causale` varchar(100) NOT NULL,
+  `Priorita` varchar(10) NOT NULL,
+  `Lingua` varchar(10) NOT NULL,
+  `Costo` int(10) DEFAULT NULL,
+  `Apertura` date NOT NULL DEFAULT current_timestamp(),
+  `Chiusura` date DEFAULT NULL,
+  `OreImpiegate` int(4) DEFAULT NULL,
+  `Cliente` int(11) NOT NULL,
+  `Macchina` int(12) DEFAULT NULL,
+  `Tecnico` int(4) NOT NULL,
+  `Cons` int(12) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`Partita_IVA`),
+  ADD UNIQUE KEY `Nome` (`Nome`);
+
+--
+-- Indici per le tabelle `consumabile`
+--
+ALTER TABLE `consumabile`
+  ADD PRIMARY KEY (`IDConsumabile`);
+
+--
+-- Indici per le tabelle `contratto`
+--
+ALTER TABLE `contratto`
+  ADD PRIMARY KEY (`IDContratto`),
+  ADD UNIQUE KEY `Cliente` (`Cliente`);
+
+--
+-- Indici per le tabelle `datianagrafici`
+--
+ALTER TABLE `datianagrafici`
+  ADD PRIMARY KEY (`CodiceFiscale`);
+
+--
+-- Indici per le tabelle `datilavorativi`
+--
+ALTER TABLE `datilavorativi`
+  ADD PRIMARY KEY (`IDTecnico`),
+  ADD UNIQUE KEY `CF` (`CF`);
+
+--
+-- Indici per le tabelle `emailcliente`
+--
+ALTER TABLE `emailcliente`
+  ADD PRIMARY KEY (`NumCliente`,`Email`);
+
+--
+-- Indici per le tabelle `emailtecnico`
+--
+ALTER TABLE `emailtecnico`
+  ADD PRIMARY KEY (`NumTecnico`,`Email`);
+
+--
+-- Indici per le tabelle `garanzia`
+--
+ALTER TABLE `garanzia`
+  ADD PRIMARY KEY (`IDGaranzia`);
+
+--
+-- Indici per le tabelle `macchinario`
+--
+ALTER TABLE `macchinario`
+  ADD PRIMARY KEY (`IDMacchinario`),
+  ADD KEY `Gar->Garanzia.IDGaranzia` (`Gar`);
+
+--
+-- Indici per le tabelle `telcliente`
+--
+ALTER TABLE `telcliente`
+  ADD PRIMARY KEY (`NumCliente`,`Telefono`);
+
+--
+-- Indici per le tabelle `teltecnico`
+--
+ALTER TABLE `teltecnico`
+  ADD PRIMARY KEY (`NumTecnico`,`Telefono`);
+
+--
+-- Indici per le tabelle `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`IDTicket`),
+  ADD KEY `Cliente->Cliente.Partita_IVA` (`Cliente`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `consumabile`
+--
+ALTER TABLE `consumabile`
+  MODIFY `IDConsumabile` int(12) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `contratto`
+--
+ALTER TABLE `contratto`
+  MODIFY `IDContratto` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `datilavorativi`
+--
+ALTER TABLE `datilavorativi`
+  MODIFY `IDTecnico` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `garanzia`
+--
+ALTER TABLE `garanzia`
+  MODIFY `IDGaranzia` int(7) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `macchinario`
+--
+ALTER TABLE `macchinario`
+  MODIFY `IDMacchinario` int(12) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `IDTicket` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `contratto`
+--
+ALTER TABLE `contratto`
+  ADD CONSTRAINT `Cliente->Cliente.PartitaIVA` FOREIGN KEY (`Cliente`) REFERENCES `cliente` (`Partita_IVA`);
+
+--
+-- Limiti per la tabella `datilavorativi`
+--
+ALTER TABLE `datilavorativi`
+  ADD CONSTRAINT `CF->DatiAnagrafici.CodiceFiscale` FOREIGN KEY (`CF`) REFERENCES `datianagrafici` (`CodiceFiscale`);
+
+--
+-- Limiti per la tabella `emailcliente`
+--
+ALTER TABLE `emailcliente`
+  ADD CONSTRAINT `NumCliente->Cliente.Partita_IVA` FOREIGN KEY (`NumCliente`) REFERENCES `cliente` (`Partita_IVA`);
+
+--
+-- Limiti per la tabella `emailtecnico`
+--
+ALTER TABLE `emailtecnico`
+  ADD CONSTRAINT `NumTecnico->DatiLavorativi.IDTecnico` FOREIGN KEY (`NumTecnico`) REFERENCES `datilavorativi` (`IDTecnico`);
+
+--
+-- Limiti per la tabella `macchinario`
+--
+ALTER TABLE `macchinario`
+  ADD CONSTRAINT `Gar->Garanzia.IDGaranzia` FOREIGN KEY (`Gar`) REFERENCES `garanzia` (`IDGaranzia`);
+
+--
+-- Limiti per la tabella `telcliente`
+--
+ALTER TABLE `telcliente`
+  ADD CONSTRAINT `NumCliente->Cliente.PartitaIVA` FOREIGN KEY (`NumCliente`) REFERENCES `cliente` (`Partita_IVA`);
+
+--
+-- Limiti per la tabella `teltecnico`
+--
+ALTER TABLE `teltecnico`
+  ADD CONSTRAINT `NumTecnico->DatiLavorativi.ID_Tecnico` FOREIGN KEY (`NumTecnico`) REFERENCES `datilavorativi` (`IDTecnico`);
+
+--
+-- Limiti per la tabella `ticket`
+--
+ALTER TABLE `ticket`
+  ADD CONSTRAINT `Cliente->Cliente.Partita_IVA` FOREIGN KEY (`Cliente`) REFERENCES `cliente` (`Partita_IVA`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

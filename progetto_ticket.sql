ff77bdf85327a3142bcb27660c1aa1474f46f481
Federico Staffolani <94174873+fedeStaffo@users.noreply.github.com>
2022-12-11 18:06:36 +0100

    aggiunti riempimenti staffo ed ems

--- progetto_ticket.sql
+++ progetto_ticket.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Dic 11, 2022 alle 16:22
+-- Creato il: Dic 11, 2022 alle 18:06
 -- Versione del server: 10.4.27-MariaDB
 -- Versione PHP: 8.1.12
 
@@ -130,10 +130,26 @@ CREATE TABLE `consumabile` (
 --
 
 INSERT INTO `consumabile` (`IDConsumabile`, `Categoria`, `Prezzo`) VALUES
-(1, 'Lampade', 15),
-(2, 'Feltri', 5),
-(3, 'feltri', 10),
-(4, 'feltri', 20);
+(1, 'Lame di taglio', 100),
+(2, 'Lampade', 25),
+(3, 'Lampade', 25),
+(4, 'Penne', 20),
+(5, 'Punzoni', 20),
+(6, 'Frese', 18),
+(7, 'Penne', 20),
+(8, 'Punzoni', 20),
+(9, 'Frese', 18),
+(10, 'Punzoni', 18),
+(11, 'PC', 300),
+(12, 'Lame di taglio', 100),
+(13, 'PC', 300),
+(14, 'Fustelle', 30),
+(15, 'Fustelle', 30),
+(16, 'Fustelle', 30),
+(17, 'Lampade', 25),
+(18, 'Fustelle', 30),
+(19, 'Penne', 20),
+(20, 'Feltri', 15);
 
 -- --------------------------------------------------------
 
@@ -161,6 +177,14 @@ INSERT INTO `contratto` (`IDContratto`, `DataInizio`, `DataFine`, `Canone`, `Ban
 (4, '2020-12-20', '2023-12-20', 7000, 'Apex Bank', '16735731623'),
 (5, '2020-12-29', '2020-12-29', 8500, 'Premium Bank', '22174266763'),
 (6, '2020-12-30', '2023-12-30', 3000, 'Premium Bank', '26041915147'),
+(7, '2021-05-08', '2024-05-11', 6000, 'Spring Financial', '54936494783'),
+(8, '2021-05-11', '2024-05-11', 300, 'Spring Financial', '28054881706'),
+(9, '2021-05-20', '2024-05-20', 100, 'Capital Trust', '76091394328'),
+(10, '2021-05-25', '2024-05-25', 3500, 'Capital Trust', '81383350644'),
+(11, '2021-07-30', '2024-07-30', 5000, 'Joint Credit Union', '33315005088'),
+(12, '2021-07-23', '2024-07-23', 8000, 'Joint Credit Union', '95202775872'),
+(13, '2021-08-16', '2024-08-16', 7600, 'Velvet Bank', '83229080007'),
+(14, '2021-09-11', '2024-09-11', 3400, 'Velvet Bank', '68249353645'),
 (15, '2022-12-11', '2025-12-11', 300, 'China Bank', '10128395405'),
 (16, '2022-08-09', '2025-08-09', 500, 'Intesa Sanpaolo', '14617938604'),
 (17, '2021-01-02', '2024-01-02', 200, 'Premium Bank', '31713344653'),
@@ -170,7 +194,13 @@ INSERT INTO `contratto` (`IDContratto`, `DataInizio`, `DataFine`, `Canone`, `Ban
 (21, '2021-03-05', '2024-03-05', 3700, 'Flow Banks', '43849287106'),
 (22, '2021-03-13', '2024-03-13', 2500, 'Flow Banks', '90452571726'),
 (23, '2021-04-18', '2024-04-18', 8500, 'Miracle Bank', '71471551684'),
-(24, '2021-04-20', '2021-04-20', 9000, 'Miracle Bank', '55429180819');
+(24, '2021-04-20', '2021-04-20', 9000, 'Miracle Bank', '55429180819'),
+(25, '2021-10-19', '2024-10-19', 5200, 'Grade Holdings', '26368001629'),
+(26, '2021-11-30', '2024-11-30', 5000, 'Grade Holdings', '93685664951'),
+(27, '2021-12-06', '2024-12-06', 800, 'Ace Bank', '99528944960'),
+(28, '2022-01-15', '2025-01-15', 7000, 'Ace Bank', '41012850653'),
+(29, '2022-03-17', '2025-03-17', 4500, 'Ace Bank', '49335238861'),
+(30, '2022-04-10', '2025-04-10', 200, 'Ace Bank', '66098192582');
 
 -- --------------------------------------------------------
 
@@ -303,6 +333,23 @@ CREATE TABLE `emailtecnico` (
   `Email` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dump dei dati per la tabella `emailtecnico`
+--
+
+INSERT INTO `emailtecnico` (`NumTecnico`, `Email`) VALUES
+(1, 'enrico.tarli@gmail.com'),
+(1, 'enricotarli.elitron@gmail.com'),
+(2, 'gcarli@gmail.com'),
+(3, 'laura.binachi@libero.it'),
+(3, 'lbianchi@gmail.com'),
+(4, 'edluca@libero.it'),
+(5, 'luigi.verdi@gmail.com'),
+(6, 'aldodraghi@alice.it'),
+(7, 'farmandi@gmail.com'),
+(7, 'franco.armandi@libero.it'),
+(8, 'm.rossi@gmail.com');
+
 -- --------------------------------------------------------
 
 --
@@ -354,6 +401,28 @@ CREATE TABLE `specializzazione` (
   `Tecnico` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dump dei dati per la tabella `specializzazione`
+--
+
+INSERT INTO `specializzazione` (`Macchina`, `Tecnico`) VALUES
+('Booster', 1),
+('Booster', 3),
+('Booster', 6),
+('Kombo STC', 2),
+('Kombo STC', 5),
+('Kombo STC', 7),
+('Kombo TAV', 4),
+('Kombo TAV', 5),
+('Kombo TAV', 7),
+('Kombo TH', 2),
+('Kombo TH', 3),
+('Kombo TH', 6),
+('Kombo TH', 7),
+('Spark', 1),
+('Spark', 4),
+('Spark', 8);
+
 -- --------------------------------------------------------
 
 --
@@ -364,6 +433,66 @@ CREATE TABLE `telcliente` (
   `NumCliente` char(11) NOT NULL,
   `Telefono` varchar(13) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `telcliente`
+--
+
+INSERT INTO `telcliente` (`NumCliente`, `Telefono`) VALUES
+('10128395405', '+867410025845'),
+('10128395405', '+868459321110'),
+('14617938604', '+395846325965'),
+('15268876497', '+338459821001'),
+('16058522218', '+421856395211'),
+('16497514769', '+458653203332'),
+('16735731623', '+351269995127'),
+('22174266763', '+396852244859'),
+('26041915147', '+396523374870'),
+('26368001629', '+448523496511'),
+('26956659580', '+390145796426'),
+('28054881706', '+111482611866'),
+('28054881706', '+177095919533'),
+('31713344653', '+169919923958'),
+('32142173559', '+138805802387'),
+('32871434744', '+167450953447'),
+('33315005088', '+492284749036'),
+('36144094207', '+393738297450'),
+('41012850653', '+385171738154'),
+('42454205237', '+420985933815'),
+('43849287106', '+398231052306'),
+('45265299447', '+390455940165'),
+('48925706556', '+398706648324'),
+('49335238861', '+379000000000'),
+('49386669284', '+966747425197'),
+('54936494783', '+903175939337'),
+('55429180819', '+396672734501'),
+('56934816291', '+912356686126'),
+('65235797314', '+816598426580'),
+('66098192582', '+352781012959'),
+('68249353645', '+148619110484'),
+('69090775976', '+398525949425'),
+('71471551684', '+180519847170'),
+('71471551684', '+180519847179'),
+('71475599444', '+278393030160'),
+('72304749147', '+886748403024'),
+('73586436589', '+399349520095'),
+('76091394328', '+398345499688'),
+('78989412550', '+137374435110'),
+('81383350644', '+394900414694'),
+('82026462441', '+166322200671'),
+('82026462441', '+166322200675'),
+('83007757185', '+352995838638'),
+('83229080007', '+431132819553'),
+('89167280676', '+395851354922'),
+('90452571726', '+640866950556'),
+('90523111782', '+212181139851'),
+('93269259186', '+394448603435'),
+('93685664951', '+370044615215'),
+('95202775872', '+355451947314'),
+('95890272415', '+372746239502'),
+('96799763888', '+115508348524'),
+('97925277933', '+640313826560'),
+('99528944960', '+433232859055');
 
 -- --------------------------------------------------------
 
@@ -508,13 +637,13 @@ ALTER TABLE `ticket`
 -- AUTO_INCREMENT per la tabella `consumabile`
 --
 ALTER TABLE `consumabile`
-  MODIFY `IDConsumabile` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `IDConsumabile` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT per la tabella `contratto`
 --
 ALTER TABLE `contratto`
-  MODIFY `IDContratto` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `IDContratto` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT per la tabella `datilavorativi`

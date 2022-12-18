68636ec4bca86607b25b96980968b9ff8c362ae9
Ems <95298646+Ems01@users.noreply.github.com>
2022-12-18 11:17:07 +0100

    aggiornamento operazioni sql

--- Operazioni.sql
+++ Operazioni.sql
@@ -1,9 +1,7 @@
 /*OPERAZIONE 1 */
 INSERT INTO `cliente` (`Partita_IVA`, `Nome`, `Stato`, `Saldo`) VALUES (...);
-
 /* Per ogni telefono posseduto dal cliente */
 INSERT INTO `telcliente` (`NumCliente`, `Telefono`) VALUES (...);
-
 /*Per ogni email posseduta dal cliente */
 INSERT INTO `emailcliente` (`NumCliente`, `Email`) VALUES (...);
 
@@ -11,16 +9,12 @@ INSERT INTO `emailcliente` (`NumCliente`, `Email`) VALUES (...);
 /*OPERAZIONE 2 */
 /* Nel campo IDTecnico non va inserito nulla in quanto il sistema aggiunge automaticamente un valore con auto_increment */
 INSERT INTO `datilavorativi` (`IDTecnico`, `Resident`, `CF`) VALUES (null, <resident>, <cod_fisc>);
-
 /* Inserimento dei dati anagrafici del tecnico */
 INSERT INTO `datianagrafici` (`CodiceFiscale`, `Nome`, `Cognome`, `Sesso`, `DataNascita`) VALUES (...);
-
 /* Per ogni telefono posseduto dal tecnico */
 INSERT INTO `teltecnico` (`NumTecnico`, `Telefono`) VALUES (...);
-
 /*Per ogni email posseduta dal tecnico */
 INSERT INTO `emailtecnico` (`NumTecnico`, `Email`) VALUES (...);
-
 /* Per ogni tecnico si inseriscono i macchinari a cui è specializzato */
 INSERT INTO `specializzazione` (`Macchina`, `Tecnico`) VALUES (...);
 
@@ -28,73 +22,69 @@ INSERT INTO `specializzazione` (`Macchina`, `Tecnico`) VALUES (...);
 /* OPERAZIONE 3 */
 /* Nel campo IDMacchinario non va inserito nulla in quanto il sistema aggiunge automaticamente un valore con auto_increment */
 INSERT INTO `macchinario` (`IDMacchinario`, `Categoria`, `Gar`) VALUES (null, <cat>, <id_garanzia>);
+/* Nel campo IDGaranzia non va inserito nulla in quanto il sistema aggiunge automaticamente un valore con auto_increment
+Il campo DataInstallazione viene settato come current_date in quanto all'installazione del macchinario coincide l'inizio della garanzia
+Il campo Scadenza invece coincide con la data di installazione traslata di un anno. */
+INSERT INTO `garanzia` (`IDGaranzia`, `DataInstallazione`, `LuogoInstallazione`, `Scadenza`) VALUES (null, current_date, <luogo>, current_date + 1 YEAR)
 
 /* OPERAZIONE 4 */
 /* Nel campo IDTicket non va inserito nulla in quanto il sistema aggiunge automaticamente un valore con auto_increment 
 Inoltre anche gli attributi costo, chiusura e oreImpiegate verranno inizializzati a null in quanto si inseriranno alla chiusura del ticket.
 Invece per quanto riguarda il campo Apertura, si inserisce current_date in quanto l'inserimento di un ticket coincide col momento dell'apertura.*/
 INSERT INTO `ticket` (`IDTicket`, `Causale`, `Priorita`, `Lingua`, `Costo`, `Apertura`, `Chiusura`, `OreImpiegate`, `Cliente`) VALUES (null, <causale>, <priorita>, <lingua>, null, current_date, null, null, <Partita_IVA>);
-
 /*Quando la causale del Ticket si riferisce ai consumabili, si andranno ad associare il ticket con il consumabile richiesto */
 INSERT INTO `assistenzacons` (`Ticket`, `Cons`) VALUES (...);
-
 /*Quando la causale del Ticket si riferisce ai macchinari, si andranno ad associare il ticket con il macchinario specifico e il tecnico specializzato
 Il campo NumOre verrà utilizzato per indicare il numero di ore del singolo tecnico e verrà inizialmente impostato come valore nullo. */
 INSERT INTO `assistenzamacc` (`Ticket`, `Macchina`, `Tecnico`, `NumOre`) VALUES (<id_ticket>, <id_macchina>, <id_tecnico>, null);
 
 
-/*OPERAZIONE 5 - DA CONTROLLARE*/
-/* Nel campo IDGaranzia non va inserito nulla in quanto il sistema aggiunge automaticamente un valore con auto_increment
-Il campo DataInstallazione viene settato come current_date in quanto all'installazione del macchinario coincide l'inizio della garanzia
-Il campo Scadenza invece coincide con la data di installazione traslata di un anno. */
-INSERT INTO `garanzia` (`IDGaranzia`, `DataInstallazione`, `LuogoInstallazione`, `Scadenza`) VALUES (null, current_date, <luogo>, current_date + 1 YEAR)
-
-/* OPERAZIONE 6 */
+/* OPERAZIONE 5 */
 /* Nel campo IDContratto non va inserito nulla in quanto il sistema aggiunge automaticamente un valore con auto_increment
 Il campo DataInizio viene settato come current_date in quanto l'inserimento di un nuovo contratto coincide con l'inizio di validità del contratto
 Il campo DataFine invece coincide con la data di inizio traslata di tre anni.  */
 INSERT INTO `contratto` (`IDContratto`, `DataInizio`, `DataFine`, `Canone`, `Banca`, `Cliente`) VALUES (null, current_date, current_date + 3 YEAR, <canone>, <banca>, <Partita_IVA>);
 
-/* OPERAZIONE 7 */
+/* OPERAZIONE 6 */
 /* Nel campo IDConsumabile non va inserito nulla in quanto il sistema aggiunge automaticamente un valore con auto_increment */
 INSERT INTO `consumabile` (`IDConsumabile`, `Categoria`, `Prezzo`) VALUES (null, <cat>, <prezzo>);
 
-/* OPERAZIONE 8 */
+/* OPERAZIONE 7 */
 /*Se si vuole cambiare il saldo  di un cliente da positivo a negativo */
 UPDATE cliente set Saldo = '-' where Partita_IVA = <p_IVA>;
-
 /*Se si vuole cambiare il saldo di un cliente da negativo a positivo */
 UPDATE cliente set Saldo = '+' where Partita_IVA = <p_IVA>;
 
-/* OPERAZIONE 9 */
+/* OPERAZIONE 8 */
 /* Se un tecnico passa da resident a non resident */
 UPDATE datilavorativi set Resident = 'No' where IDTecnico = <id_tecnico>;
-
 /* Se un tecnico passa da non resident a resident */
 UPDATE datilavorativi set Resident = 'Si' where IDTecnico = <id_tecnico>;
 
-/* OPERAZIONE 10 ???*/
+/* OPERAZIONE 9 */
+/* Se un tecnico sbaglia a mettere la categoria di un macchinario*/
+UPDATE macchinario set Categoria = <cat> where IDMacchinario = <id_macchina>;
 
-
-/* OPERAZIONE 11 DA CONTROLLARE PER CONSUMABILE */
+/* OPERAZIONE 10 DA CONTROLLARE PER CONSUMABILE */
 UPDATE ticket set Causale = <causale> where IDTicket = <id_ticket>;
 
-/* OPERAZIONE 12 */
+/* OPERAZIONE 11 */
 UPDATE consumabile set Prezzo = <prezzo> where IDConsumabile = <id_consumabile>;
 
-/* OPERAZIONE 13 */
+/* OPERAZIONE 12 da controllare per telefoni ed email*/
 DELETE FROM cliente WHERE Partita_IVA = <p_IVA>;
 
-/* OPERAZIONE 14 */
+/* OPERAZIONE 13 da controllare per telefoni ed email*/
 DELETE FROM datilavorativi WHERE CF = <cod_fisc>;
-
 DELETE FROM datianagrafici WHERE CodiceFiscale = <cod_fisc>;
 
-/* OPERAZIONE 15 ???? */
+/* OPERAZIONE 14 ???? */
 DELETE FROM macchinario WHERE IDMacchinario = <id_macchina>;
 
-/* OPERAZIONE 16 ???? */
+/* OPERAZIONE 15 ???? */
 DELETE FROM consumabile WHERE IDConsumabile = <id_consumabile>;
+
+/* OPERAZIONE 16 */
 
 /* OPERAZIONE 17 */
 
@@ -145,7 +135,6 @@ FROM cliente
 JOIN acquistomacc ON acquistomacc.Cliente = cliente.Partita_IVA 
 JOIN macchinario ON acquistomacc.Macchinario = macchinario.IDMacchinario 
 WHERE cliente.Partita_IVA = '<partita_iva>';
-
 /*Se si vogliono ricercare le macchine di un cliente utilizzando il nome del cliente come chiave di ricerca */
 SELECT cliente.Partita_IVA, cliente.Nome, macchinario.IDMacchinario, macchinario.Categoria 
 FROM cliente 
@@ -159,7 +148,6 @@ SELECT ticket.*, cliente.Nome
 FROM ticket 
 JOIN cliente ON ticket.Cliente = cliente.Partita_IVA 
 WHERE cliente.Partita_IVA = '<partita_iva>';
-
 /*Se si vogliono ricercare i ticket di un cliente utilizzando il nome del cliente come chiave di ricerca */
 SELECT ticket.*, cliente.Nome 
 FROM ticket 
